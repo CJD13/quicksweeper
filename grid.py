fa8ce173df0112888ac80e3c65dd48acee8d911b
Caleb Dastrup <calebjdastrup@gmail.com>
2023-05-12 19:18:35 -0400

    Added grid support

---
+++ grid.py
@@ -0,0 +1,113 @@
+import vector
+import itertools
+#File supporting grids of squares.
+class grid:
+    def __init__(self, get=lambda x, y: None, s=lambda x, y, v: None, setRow=lambda x, r: None):
+        self.get=get
+        self.set=s
+    def of(array2):
+        #self.data=array2
+        def get(x, y):
+            if 0<=x and x<len(array2) and 0<=y and y<=len(array2[x]):
+                return array2[x][y]
+            return None
+        def s(x,y,v):
+            array2[x][y]=v
+        def setRow(x, r):
+            array2[x]=r
+        g=grid(get, s)
+        return finiteGrid(g, 0, len(array2)-1,0,max(len(k)-1 for k in array2))
+    def contains(self,x,y):
+        return self[x][y]!=None
+    #returns this grid represented with a new coordinate system, centered at (x,y)
+    def recentered(self,x,y):
+        return grid(get=lambda a, b: self[a-x][b-y])
+    #returns this grid translated by (dx,dy)
+    def translated(self, dx, dy):
+        return self.recentered(-dx,-dy)
+    #returns this grid rotated counterclockwise by an angle of n pi/2
+    def rotated(self,n):
+        rotX=vector([[1,0],[0,-1],[-1,0],[0,1]][n%4])
+        rotY=vector([[0,1],[1,0],[0,-1],[-1,0]][n%4])
+        return grid(get=lambda x, y: self[vector(x,y).dot(rotX)][vector(x,y).dot(rotY)])
+    #reflects over the line y=x
+    def reflected(self):
+        return grid(get=lambda x, y: self[y][x])
+    #To conform with the semantics for arrays, slicing with a specified start point will move the coordinate system to place 0 at that point, and slicing with no start will keep the origin unchanged.
+    def __getitem__(self, item):
+        if type(item)==slice:
+            offset = 0 if item.start==None else item.start
+            step = 1 if item.step==None else item.step
+            def get(x,y):
+                x=x*step+offset
+                if (item.start!=None and x<start) or (item.stop!=None and x>=item.stop):
+                    return None
+                return self[x,y]
+            return grid(get=get).reflected()
+        if type(item)==tuple:
+            return self.get(item[0],item[1])
+        return gridrow(get=lambda y: self.get(item,y),s=lambda y, v: self.set(x,y,v))
+    def __setitem__(self, item, value):
+        self.setRow(item,value)
+class gridrow:
+    def __init__(self, get=lambda x: None, s=lambda x, v: None):
+        self.get=get
+        self.set=s
+    def __getitem__(self,item):
+        return self.get(item)
+    def __setitem__(self,item,value):
+        self.set(item, value)
+#A grid with a bounding rectangle
+class finiteGrid:
+    def __init__(self, grid, lx, rx, ly, uy):
+        self.grid=grid
+        self.bL=lx
+        self.bR=rx
+        self.bD=ly
+        self.bU=uy
+    def contains(self,x,y):
+        return self.grid.contains(x,y)
+    def recentered(self,x,y):
+        return finiteGrid(self.grid.recentered(x,y),self.bL-x,self.bR-x,self.bD-y,self.bU-y)
+    def translated(self,x,y):
+        return self.recentered(-dx,-dy)
+    def rotated(self,n):
+        l=[self.bR,-self.bD,-self.bL,self.bR]
+        return finiteGrid(self.grid.rotated(n), -l[(n+2)%4],l[(n+3)%4],-l[(n+1)%4],l[(n+0)%4])
+    def reflected(self):
+        return finiteGrid(self.grid.reflected(),self.bD,self.bU,self.bL,self.bR)
+    #After slicing, the origin will be at the bottom-left of the slice (conforms with array semantics)
+    def __getitem__(self,item):
+        if type(item)==slice:
+            start = self.bL if item.start==None else item.start
+            stop = self.bR+1 if item.stop==None else item.stop
+            step = 1 if item.step==None else item.step
+            def get(x,y):
+                x=x*step+offset
+                if x<start or x>=stop:
+                    return None
+                return self[x,y]
+            return finiteGrid(grid,start,stop,self.bD,self.bU).reflected()
+        if type(item)==tuple:
+            return self.get(item[0],item[1])
+        return gridrow(get=lambda y: self.get(item,y),s=lambda y, v: self.set(x,y,v))
+    def set(x,y,v):
+        self.grid.set(x,y,v)
+        self.bL=min(self.bL,x)
+        self.bR=max(self.bR,x)
+        self.bD=min(self.bD,y)
+        self.bU=max(self.bU,y)
+    def __iter__(self):
+        return (i for i in (self.grid[x][y] for (x,y) in itertools.product(range(self.bL,self.bR+1),range(self.bD,self.bU+1))) if i!=None)
+        #(self.bL..self.bR).product(self.bD..self.bU).map(|(x,y)| self.grid[x][y]).flatten()
+    def __str__(self):
+        res=''
+        for y in range(self.bU,self.bL-1,-1):
+            for x in range(self.bL,self.bR+1):
+                res+=str(self[x][y])+'\t'
+            res+='\n'
+        return res
+class gridIterator:
+    def __init__(self, g):
+        self.grid=grid
+        self.i=grid
--- tunnels.py
+++ tunnels.py
@@ -1,5 +1,54 @@
 import base
 import maze
 import minesweeper
+import math
+#returns all squares which can be seen from the point at the center of square (x,y)
 def lineOfSight(grid,x,y,unblocked=lambda a: a==0 or a==1):
-    
+    if not unblocked(grid[x][y]): return []
+    slopes=[(-1,1)]
+    d=1
+    newSlopes=[]
+    res=[(x,y)]
+    while len(slopes)>0 and x+d<len(grid):
+        print(slopes)
+        for (l,u) in slopes:
+            i=max(-y, math.floor((d+.5 if l<0 else d-.5)*l+.5))
+            ui=min(len(grid)-y-1,math.ceil((d+.5 if u>0 else d-.5)*u-.5))
+            print((i,ui))
+            while i<=ui:
+                if unblocked(grid[x+d][y+i]):
+                    res.append((x+d,y+i))
+                else:
+                    nu = (i-.5)/(d-.5) if i<=0 else (i-.5)/(d+.5)
+                    if nu>l:
+                        newSlopes.append((l,nu))
+                    l = (i+.5)/(d-.5) if i>=0 else (i+.5)/(d+.5)
+                i+=1
+            if u>l:
+                newSlopes.append((l,u))
+        d+=1
+        slopes=newSlopes
+        newSlopes=[]
+    return res
+'''grid = [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,1,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0]]
+for (i,j) in lineOfSight(grid,0,3,lambda a: a==0):
+    grid[i][j]=2
+for l in grid:
+    print(l)'''
+import random
+grid=[[0]*201 for i in range(201)]
+for i in range(5000):
+    grid[random.randint(0,200)][random.randint(0,200)]=1
+grid[100][100]=0
+for (i,j) in lineOfSight(grid,100,100,lambda a: a==0):
+    grid[i][j]=2
+for l in grid:
+    print(str(l)+',')
+
+        
