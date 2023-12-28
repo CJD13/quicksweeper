17c5a14dfa3c45145c29a37a5361462fd5792764
zbrachinara <zbrachinara@gmail.com>
2023-12-27 19:54:56 -0500

    make some types for representing server state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{collections::HashMap, net::TcpStream};
+
+use glam::IVec2;
+use tungstenite::WebSocket;
+
+struct Server {
+    games: Vec<AreaAttack>,
+}
+
+enum TileKind {
+    Empty { neighbors: u8 },
+    Mine,
+}
+
+struct Tile {
+    kind: TileKind,
+    owner: Option<PlayerId>,
+}
+
+enum AreaAttackEvent {
+    Join {},
+    Revealed {},
+    TileClaimed {},
+    Frozen {},
+    Message {},
+    StateChange {},
+}
+
+struct AreaAttack {
+    players: HashMap<PlayerId, WebSocket<TcpStream>>,
+    board: HashMap<IVec2, Tile>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+struct PlayerId {
+    id: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
