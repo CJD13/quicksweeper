a99b3d0f4b7d9521fe537d62a95eae901959ec2c
zbrachinara <zbrachinara@gmail.com>
2024-01-18 09:15:40 -0500

    basic tcp watching

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,44 @@
-use std::{collections::HashMap, net::TcpStream, fmt::Error};
+use std::{collections::HashMap, io, net::TcpStream};
+use std::net::TcpListener;
+
 mod minesweep_grid;
 mod area_attack;
-use glam::IVec2;
+
 use tungstenite::WebSocket;
+
 trait Ruleset {
     //Adds a player.
     fn add(&mut self, id: PlayerId) -> Vec<(PlayerId, Vec<u8>)>;
-    fn update_state(&mut self)-> Vec<(PlayerId, Vec<u8>)>;
+    fn update_state(&mut self) -> Vec<(PlayerId, Vec<u8>)>;
     //Returned is a list of messages to be sent to the corresponding players.
     fn process_data(&mut self, id: PlayerId, data: Vec<u8>) -> Vec<(PlayerId, Vec<u8>)>;
 }
+
 struct Game {
     players: Vec<Player>,
-    ruleset: Box<dyn Ruleset>
+    ruleset: Box<dyn Ruleset>,
 }
+
+#[derive(Default)]
 struct Server {
-    games: HashMap<GameId,Game>,
+    uninitialized: Vec<TcpStream>,
+    waiting: Vec<Player>,
+    games: HashMap<GameId, Game>,
 }
 
 struct GameId {
-    id: u32
+    id: u32,
 }
+
 struct Player {
     name: String,
     id: PlayerId,
-    connection: WebSocket<TcpStream>
+    connection: WebSocket<TcpStream>,
 }
+
 impl PartialEq for Player {
     fn eq(&self, other: &Self) -> bool {
-        self.id==other.id
+        self.id == other.id
     }
 }
 
@@ -38,5 +48,33 @@ struct PlayerId {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut server = Server::default();
+
+    let address = "localhost:31111";
+    let mut socket = TcpListener::bind(address).unwrap();
+    socket.set_nonblocking(true).expect("Could not accept nonblocking tcp connections");
+
+    loop {
+        for stream in socket.incoming() {
+            match stream {
+                Ok(stream) => {
+                    server.uninitialized.push(stream);
+                }
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    // continue
+                }
+                Err(_) => {
+                    //TODO log the error
+                }
+            }
+        }
+
+        for (id, game) in &mut server.games {
+            game.ruleset.update_state();
+            for player in &mut game.players {
+                //TODO process data
+            }
+        }
+    }
 }
+
